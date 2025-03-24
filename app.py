fb76f62cae30c354f6dbc3082f90e5841048f903
ADITYAAV80 <adityaav80@gmail.com>
2025-03-24 16:17:43 +0530

    updated requirements.txt

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import os
 from langchain_groq import ChatGroq
 from langchain_openai import OpenAIEmbeddings
-from langchain_huggingface import HuggingFaceEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains.combine_documents.stuff import create_stuff_documents_chain
 from langchain.chains import create_retrieval_chain
